55e59f3ea547aa4e89f8ac18c62a760704c26b77
jibril <100243649+09jayy@users.noreply.github.com>
2025-03-08 15:50:08 +0000

    Created descriptions for App.jsx, main.jsx - add Home.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+/**
+ * Handles global layout and routes to other pages/components
+ * - Contains navigation to different pages/components via a Top menu bar
+ * - Actual links to pages/components
+ * @returns JSX
+ */
 function App() {
   const [count, setCount] = useState(0)
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/**
+ * Entry file where React is initialised and rendered into the DOM
+ */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
