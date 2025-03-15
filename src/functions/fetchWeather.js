71dea5c5b17257dd36705fe6e34d372600c965ee
jibril <100243649+09jayy@users.noreply.github.com>
2025-03-15 03:19:00 +0000

    fixed fetchWeather.js function
    useCallback is invalid used outside of a react component function

--- src/functions/fetchWeather.js
+++ src/functions/fetchWeather.js
@@ -1,14 +1,11 @@
-import { useCallback } from "react";
+import axios from 'axios';
 
-export const fetchWeather = (city) => {
-    const fetchData = useCallback(async () => {
-        try {
-            const response = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=ca7fa4eea17f1ddca4179ea69c71470b`
-            );
-            return response
-        } catch (err) {
-            return response
-        }
-    })
+export const fetchWeather = async (city) =>  { 
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=ca7fa4eea17f1ddca4179ea69c71470b`); 
+        return response.data; 
+    } catch (error) {
+        console.error(error);
+        return null; 
+    }
 }
