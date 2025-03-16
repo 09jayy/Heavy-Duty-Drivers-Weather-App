5c1ae7f6a3bd3f72a8087c8dcc9fd8289729795d
jibril <100243649+09jayy@users.noreply.github.com>
2025-03-16 02:54:04 +0000

    city is now added when pressing enter key

--- src/pages/weather/components/SearchModal.jsx
+++ src/pages/weather/components/SearchModal.jsx
@@ -6,12 +6,25 @@ export const SearchModal = ({onClose, onSubmit}) => {
 
     const [userLocation, setUserLocation] = useState(''); 
 
+    const handleKeyDown = (event) => {
+        if (event.key == 'Enter') {
+            event.preventDefault(); 
+            onSubmit(userLocation); 
+            onClose(); 
+        }
+    }
+
     return (
         <div className='modal-overlay'>
             <div className='modal-content'>
                 <button onClick={onClose}>X</button>
                 Search Modal
-                <input value={userLocation} onChange={(e) => setUserLocation(e.target.value)} placeholder={'City...'}/>
+                <input 
+                    value={userLocation} 
+                    onChange={(e) => setUserLocation(e.target.value)} 
+                    placeholder={'City...'}
+                    onKeyDown={handleKeyDown}
+                />
                 <button onClick={()=> {onClose(); onSubmit(userLocation); }}>Add</button>
             </div>
         </div>
