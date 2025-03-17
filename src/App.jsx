9df12023e1422d91b9537bf5fab28321b3924929
jibril <100243649+09jayy@users.noreply.github.com>
2025-03-17 22:33:11 +0000

    integrated navbar in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { WeatherPage } from './pages/weather/Weather';
+import { NavBar } from './components/NavBar';
 // import './App.css';
 
 /**
@@ -12,15 +13,8 @@ import { WeatherPage } from './pages/weather/Weather';
 const App = () => {
   return (
     <div className="min-h-screen bg-[#0D1B2A]">
-      <header className="py-8 bg-gradient-to-r from-[#1B263B] to-[#273B54] shadow-lg">
-        <div className="max-w-7xl mx-auto px-6">
-          <h1 className="text-4xl font-bold text-center text-[#E0E1DD] mb-2">
-            Weather Dashboard
-          </h1>
-          <p className="text-[#778DA9] text-center max-w-xl mx-auto">
-            Get real-time weather updates for your favorite locations around the world
-          </p>
-        </div>
+      <header>
+        <NavBar/>
       </header>
       <main>
         <WeatherPage />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
     const [curPage, setCurPage] = useState('home'); 
 
     return (
-        <div>
+        <div className="py-8 bg-gradient-to-r from-[#1B263B] to-[#273B54] shadow-lg">
             <button onClick={()=>setCurPage('home')}>
                 <House/>
             </button>
